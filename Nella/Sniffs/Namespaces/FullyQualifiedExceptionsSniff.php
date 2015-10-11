5ede275a9502fdae6be2d26b55323ef1a4d241fc
Patrik Votocek <patrik@votocek.cz>
2015-10-10 21:15:08 -0400

    Travis: tuning

--- Nella/Sniffs/Namespaces/FullyQualifiedExceptionsSniff.php
+++ Nella/Sniffs/Namespaces/FullyQualifiedExceptionsSniff.php
@@ -1,27 +1,25 @@
 <?php
 /**
-* This file is part of the Nella Project (http://nella-project.org).
-*
-* Copyright (c) Patrik Votoček (http://patrik.votocek.cz)
-*
-* For the full copyright and license information,
-* please view the file LICENSE.md that was distributed with this source code.
-*/
+ * This file is part of the Nella Project (http://nella-project.org).
+ *
+ * Copyright (c) Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * For the full copyright and license information,
+ * please view the file LICENSE.md that was distributed with this source code.
+ */
 
 namespace Nella\Sniffs\Namespaces;
 
 class FullyQualifiedExceptionsSniff implements \PHP_CodeSniffer_Sniff
 {
 
-
 	/**
 	 * @return array
 	 */
 	public function register()
 	{
-		return array(T_THROW);
+		return [T_THROW];
 	}
-
 
 	/**
 	 * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
